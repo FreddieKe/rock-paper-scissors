6dc8d1dec315c07e2ed769b1dbbd1077f95ba716
Freddie <freddie.kenyon98@gmail.com>
2024-06-16 18:08:24 +0100

    Add basic UI functionality
    Adds buttons to allow player to make choice, also removing the
    five-round loop for a game.

--- myscript.js
+++ myscript.js
@@ -1,4 +1,7 @@
-// Obtain choice from computer
+// Set initial scores
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choiceNum = Math.floor(Math.random() * 3) + 1;
     console.log(choiceNum)
@@ -8,28 +11,11 @@ function getComputerChoice() {
         case 1: return "paper"
     }
 }
-// Obtain choice from player
-function getPlayerChoice() {
-    let keepGoing = 1;
-    while (keepGoing === 1) {
-        let playerChoice = prompt("Rock, paper, or scissors?").toLowerCase();
-        if (playerChoice === "rock"
-            || playerChoice === "paper" 
-            || playerChoice === "scissors"
-        ) return playerChoice;
-        else (alert("I did not understand that."))
-    }
-}
 
-// Set initial scores
-let playerScore = 0;
-let computerScore = 0;
-
-
-function playRound() {
+function playRound(buttonClicked) {
     let computerChoice = getComputerChoice();
     console.log(computerChoice);
-    let playerChoice = getPlayerChoice();
+    let playerChoice = buttonClicked;
     console.log(playerChoice);
     compareChoices(computerChoice,playerChoice);   
 }
@@ -37,7 +23,7 @@ function playRound() {
 function compareChoices(computerChoice,playerChoice) {
 // Check for draw
    if (computerChoice === playerChoice) {
-    return alert("It's a draw!");
+    return console.log("It's a draw!");
    }
 //    Check if computer won
    if ((computerChoice === "paper"
@@ -47,31 +33,28 @@ function compareChoices(computerChoice,playerChoice) {
         || (computerChoice === "scissors"
         && playerChoice === "paper"))
     // If computer won alert player
-        {alert(`You lose! ${computerChoice} beats ${playerChoice}`);
+        { console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
     // If computer won increase computer
       return computerScore++;}
 // Player must have won so alert player
     else {
-        alert(`You won! ${playerChoice} beats ${computerChoice}`);
+        console.log(`You won! ${playerChoice} beats ${computerChoice}`);
         return playerScore++;}
 }
-function playGame() {
-    // Check if computer or player has won
-    while (
-        playerScore < 5
-        && computerScore < 5
-    ) {
-        playRound();
-    // Tell which player has won
-    } if (playerScore === 5) {
-        alert(`You won! The score was player: ${playerScore} computer: ${computerScore}`)
-    } else {alert(`You lost! The score was player: ${playerScore} computer: ${computerScore}`);
-    resetScores();
-}}
 
 function resetScores() {
     playerScore = 0;
     computerScore = 0;
 }
 
-playGame();
+// Create variables for buttons
+let buttonContainer = document.querySelector(".button-container");
+let rockButton = document.querySelector("#rock");
+let paperButton = document.querySelector("#paper");
+let scissorsButton = document.querySelector("#scissors");
+
+// Set buttons to start rounds
+
+buttonContainer.addEventListener("click", (event) => playRound(event.target.id))
+
+
